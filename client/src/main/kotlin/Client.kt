0e0a2c6351ba87cf9aff5495592609f6d5be7130
Federico Barcelona <fede_rico_94@hotmail.com>
2021-10-16 19:33:00 +0200

    feat: Allow to specify the text from STDIN

--- client/src/main/kotlin/Client.kt
+++ client/src/main/kotlin/Client.kt
@@ -43,7 +43,7 @@ class Client {
      */
     @Bean
     fun lookup(translatorClient: TranslatorClient) = CommandLineRunner {
-        val input = "Translate me!"
+        val input = readLine().orEmpty().ifEmpty{ "Translate me!" }
         val request = TranslationRequest().apply {
             langFrom = "en"
             langTo = "es"
